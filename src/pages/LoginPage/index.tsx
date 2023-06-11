a3886f8f17f16cc01bcdc1ec85b8f60f608c95a7
Sang jin Lee <w1004123@gmail.com>
2023-06-11 15:34:35 +0900

    Change TsFile

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -10,33 +10,42 @@ import { useRecoilState } from 'recoil';
 import { accessGoogle } from '../../apis/recoil';
 
 const App: React.FC = () => {
-  const [user, setUser] = useState({}); 
+  const [user, setUser] = useState<Record<string, any>>({}); 
 
-  const handleCallbackResponse = (response) => {
+  const handleCallbackResponse = (response: any) => {
     console.log("Encoded JWT ID token: " + response.credential);
-    let userObject = jwt_decode(response.credential);
+    let userObject: any = jwt_decode(response.credential);
     console.log(userObject);
     setUser(userObject);
   }
 
-  const handleSignOut = () => {
+  const handleSignOut: React.MouseEventHandler<HTMLButtonElement> = () => {
     setUser({});
     alert('로그아웃 되었습니다.');
   }
 
   useEffect(() => {
-    /* global google */
-    google.accounts.id.initialize({
-      client_id: "424694176954-if5tl2kngvuu0h5ogfm8kl37p2ide6v1.apps.googleusercontent.com",
-      callback: handleCallbackResponse
-    })
+    const loadGoogleAPI = () => {
+      const script = document.createElement('script');
+      script.src = 'https://accounts.google.com/gsi/client';
+      script.onload = () => {
+        // Google API 스크립트가 로드된 후에 초기화 코드 실행
+        (window as any).google.accounts.id.initialize({
+          client_id: "424694176954-if5tl2kngvuu0h5ogfm8kl37p2ide6v1.apps.googleusercontent.com",
+          callback: handleCallbackResponse
+        });
 
-    google.accounts.id.renderButton(
-      document.getElementById("GoogleAccounts"),
-      { theme: "outline", width: "500px", height: "70px" }
-    );
+        (window as any).google.accounts.id.renderButton(
+          document.getElementById("GoogleAccount"),
+          { theme: "outline", width: "500px", height: "70px" }
+        );
 
-    google.accounts.id.prompt();
+        (window as any).google.accounts.id.prompt();
+      };
+      document.body.appendChild(script);
+    };
+
+    loadGoogleAPI();
   }, []);
 
   const [profile, setProfile] = useRecoilState(accessGoogle);
@@ -63,15 +72,15 @@ const App: React.FC = () => {
       <S.Button onClick={showModal}>
         {user.name === undefined ? "Login" : user.name}
       </S.Button>
-      <Modal open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+      <Modal visible={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
         <S.LoginModal>
           <S.LogoPages>
             <S.Logo src={Logo} alt="img" />
           </S.LogoPages>
           <S.GooglePage>
-            <div id="GoogleAccounts"></div>
+            <div id="GoogleAccount"></div>
             { Object.keys(user).length !== 0 &&
-              <S.LogOutButtons onClick={(e) => handleSignOut(e)}>Sign Out</S.LogOutButtons>
+              <S.LogOutButtons onClick={handleSignOut}>Logout</S.LogOutButtons>
             }
           </S.GooglePage>
           <S.GooglePage>
