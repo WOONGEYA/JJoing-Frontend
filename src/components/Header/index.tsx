14202d00cfbca3cad1094bf632c226fdf7b6e84b
sebanimm <gks32114221@gmail.com>
2023-06-12 19:27:27 +0900

    Revert "FIX : eslint errors"
    This reverts commit 19cdba1c6bc999196396908e956a3d955c1a8d3b.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,46 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import * as S from './style';
-import Logo from '../../assets/logo.svg';
-import Bell from '../../assets/bell.svg';
-import Profile from '../../assets/profile.svg';
+import React from 'react'
+import * as S from './style' 
+import Logo from '../../assets/logo.svg'
+import Bell from '../../assets/bell.svg'
+import Profile from '../../assets/profile.svg'
+import { useState, useEffect } from 'react'
 
 export default function Header() {
-	const [show, setShow] = useState<boolean>(false);
+  const [show, setShow] = useState<boolean>(false)
 
-	useEffect(() => {
-		const handleScroll = () => {
-			if (window.scrollY > 60) {
-				setShow(true);
-			} else {
-				setShow(false);
-			}
-		};
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if(window.scrollY > 60){
+        setShow(true);
+      } else{
+        setShow(false);
+      }
+    })
+    
+    return () => {
+      window.removeEventListener('scroll', () => {});
+    }
+  }, [])
 
-		window.addEventListener('scroll', handleScroll);
-
-		return () => {
-			window.removeEventListener('scroll', handleScroll);
-		};
-	}, []);
-
-	return (
-		<S.HeaderContainer show={show}>
-			<S.HeaderWrapper>
-				<S.LogoContainer>
-					<S.ImageLogo src={Logo} alt='logo' />
-				</S.LogoContainer>
-				<S.MenuList>
-					<S.MenuItem1>둘러보기</S.MenuItem1>
-					<S.MenuItem>새 프로젝트</S.MenuItem>
-					<S.MenuItem>채팅</S.MenuItem>
-				</S.MenuList>
-				<S.AlarmContainer>
-					<S.Image src={Bell} alt='alarm' />
-				</S.AlarmContainer>
-				<S.ProfileContainer>
-					<S.Image src={Profile} alt='profile' />
-				</S.ProfileContainer>
-			</S.HeaderWrapper>
-		</S.HeaderContainer>
-	);
+  return (
+    <S.HeaderContainer show={show}>
+      <S.HeaderWrapper>
+        <S.LogoContainer>
+          <S.ImageLogo src={Logo} alt="logo" />
+        </S.LogoContainer>
+        <S.MenuList>
+          <S.MenuItem1>둘러보기</S.MenuItem1>
+          <S.MenuItem>새 프로젝트</S.MenuItem>
+          <S.MenuItem>채팅</S.MenuItem>
+        </S.MenuList>
+        <S.AlarmContainer>
+          <S.Image src={Bell} alt="alarm" />
+        </S.AlarmContainer>
+        <S.ProfileContainer>
+          <S.Image src={Profile} alt="profile"/>
+        </S.ProfileContainer>
+      </S.HeaderWrapper>
+    </S.HeaderContainer>
+  );
 }
