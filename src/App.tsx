8472016507b0d795d7bb0af30c72646993be5884
sebanimm <gks32114221@gmail.com>
2023-06-12 19:38:11 +0900

    FIX

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import Header from './components/Header'
-import Footer from './components/footer'
-import './styles/globalStyle.css'
 
 const App = () => {
 	return (
-		<>
-			<Header />
-			<Footer />
-		</>
+		<div>
+			
+		</div>
 	);
 };
 
