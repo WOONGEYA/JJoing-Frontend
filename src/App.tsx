baa07ddbb1ede81b9ca2f3c6d6f02d7ed6b5c43f
HeeSungLee <102288399+lheesung@users.noreply.github.com>
2023-07-09 20:09:12 +0900

    DELETE:List

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Header from './components/Header'
-import List from './components/List'
 import './styles/globalStyle.css'
 
 const App = () => {
 	return (
 		<>
 			<Header />
-			<List />
 		</>
 	);
 };
