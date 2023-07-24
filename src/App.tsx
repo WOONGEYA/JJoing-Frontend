d6982d21095b77cfa9a5aa3b57e3cc64a40faa53
HeeSungLee <102288399+lheesung@users.noreply.github.com>
2023-07-24 18:21:58 +0900

    FIX:App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
-	return (
-		<div>
-
-		</div>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
