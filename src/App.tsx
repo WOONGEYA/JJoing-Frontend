e95650ab6d9b764a17cdb74e6e3f8441c7f08b07
lsj0202 <w1004123@gmail.com>
2023-07-29 00:59:08 +0900

    REFACTOR : 넓이 높이 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from 'components/Header';
 
 const App = () => {
   const queryClient = new QueryClient();
@@ -8,7 +8,9 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Routes></Routes>
+        <Routes>
+          <Route path='/' element={<Header />} />
+        </Routes>
       </BrowserRouter>
     </QueryClientProvider>
   );
