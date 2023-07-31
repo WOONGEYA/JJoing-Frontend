89a475ca7e0ddebb762536beb99ab1b6f73247d2
HeeSungLee <102288399+lheesung@users.noreply.github.com>
2023-07-31 15:43:02 +0900

    ADD:QueryClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import Detail from 'pages/Detail';
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const queryClient = new QueryClient();
 
 const App = () => {
 
   return (
-    <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
       <Routes>
         <Route path='/detail' element={<Detail />}/>
       </Routes>
     </BrowserRouter>
+    </QueryClientProvider>
   );
 };
 
