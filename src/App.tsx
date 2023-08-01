56d61a710babdb5572aac4ecc22899d01e359195
Ubinquitous <ubinquitous1@gmail.com>
2023-08-02 04:09:38 +0900

    UPDATE : 리코일 루트, 모달 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MyPage from 'pages/MyPage';
 import LoginPage from 'pages/LoginPage';
 import Detail from 'pages/Detail';
+import Modal from 'components/Modal';
+import MyPage from 'pages/myPage';
+import { RecoilRoot } from 'recoil';
 
 const queryClient = new QueryClient();
 
 const App = () => {
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<MyPage />} />
-          <Route path='/login/oauth2/code/google' element={<LoginPage />} />
-          <Route path='/detail' element={<Detail />}/>
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <Modal />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<MyPage />} />
+            <Route path='/login/oauth2/code/google' element={<LoginPage />} />
+            <Route path='/detail' element={<Detail />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </RecoilRoot>
   );
 };
 
