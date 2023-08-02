f4203f2cd99762eb75c645729b11bc4f99be798d
lsj0202 <w1004123@gmail.com>
2023-08-03 03:16:17 +0900

    FEAT: add OthersPage

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from 'pages/LoginPage';
 import Detail from 'pages/Detail';
 import Modal from 'components/Modal';
-import MyPage from 'pages/MyPage';
+import OthersPage from 'pages/OthersPage';
 import { RecoilRoot } from 'recoil';
 
 const queryClient = new QueryClient();
@@ -16,7 +16,7 @@ const App = () => {
         <Modal />
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={<MyPage />} />
+            <Route path='/' element={<OthersPage />} />
             <Route path='/login/oauth2/code/google' element={<LoginPage />} />
             <Route path='/detail' element={<Detail />} />
           </Routes>
