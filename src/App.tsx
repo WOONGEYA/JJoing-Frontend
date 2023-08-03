9427f64195d50f2624bd724eeef2e1b40641c6c0
sebanimm <gks32114221@gmail.com>
2023-08-03 10:27:16 +0900

    ADD : explore routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 import LoginPage from 'pages/LoginPage';
 import Detail from 'pages/Detail';
 import Modal from 'components/Modal';
 import MyPage from 'pages/myPage';
-import { RecoilRoot } from 'recoil';
+import Explore from 'pages/Explore';
 
 const queryClient = new QueryClient();
 
@@ -19,6 +20,7 @@ const App = () => {
             <Route path='/' element={<MyPage />} />
             <Route path='/login/oauth2/code/google' element={<LoginPage />} />
             <Route path='/detail' element={<Detail />} />
+            <Route path='/explore' element={<Explore />} />
           </Routes>
         </BrowserRouter>
       </QueryClientProvider>
