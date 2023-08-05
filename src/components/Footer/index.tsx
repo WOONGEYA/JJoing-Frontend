ed8974acea3a15b915d7e66fa7432154bb6f92dd
sebanimm <102154824+sebanimm@users.noreply.github.com>
2023-08-06 03:08:01 +0900

    FIX : 푸터 복원

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,49 @@
+import * as S from './style';
+import Logo from 'assets/logo.svg';
+
+const Footer = () => {
+  const { service, headline, tel, mail, serviceInfo, locate, copyright } = {
+    service: '소마고 연합 사이드 프로젝트 모집 플랫폼',
+    headline: ['이용약관', '개인정보처리방침', '공지사항', '문의하기', 'FAQ | QNA'],
+    tel: '010 - 1234 - 5678',
+    mail: 'jjoing.woong@gmail.com',
+    serviceInfo: '강웅빈 . 고윤영 . 이상진 . 이희성 . 한예준',
+    locate: '부산광역시 강서구 가락대로 1393',
+    copyright: 'Copyright Woong ltd. Alright reserved.',
+  };
+
+  return (
+    <S.Container>
+      <S.Logo src={Logo} />
+      <S.List>
+        <S.Title>{service}</S.Title>
+        <S.HeadLine>
+          {headline.map((item, index) => (
+            <S.HeadLineElement key={index}>{item}</S.HeadLineElement>
+          ))}
+        </S.HeadLine>
+      </S.List>
+      <S.Info>
+        <h2>Tel:</h2>
+        <span>{tel}</span>
+      </S.Info>
+      <S.Info>
+        <h2>E-mail:</h2>
+        <span>{mail}</span>
+      </S.Info>
+      <S.Info>
+        <h2>공동대표:</h2>
+        <span>{serviceInfo}</span>
+      </S.Info>
+      <S.Flex>
+        <S.Info>
+          <h2>위치:</h2>
+          <span>{locate}</span>
+        </S.Info>
+        <S.CopyRight>{copyright}</S.CopyRight>
+      </S.Flex>
+    </S.Container>
+  );
+};
+
+export default Footer;
---
+++ src/components/Footer/style.ts
@@ -0,0 +1,79 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 300px;
+  background-color: #f7f8f9;
+  padding: 60px 200px 25px;
+  box-sizing: border-box;
+  font-family: Pretendard;
+`;
+
+export const Logo = styled.img`
+  width: 122px;
+`;
+
+export const List = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid #9ea4aa;
+  margin-bottom: 24px;
+`;
+
+export const Title = styled.h2`
+  color: rgba(38, 68, 102, 0.8);
+  font-weight: 700;
+  font-size: 18px;
+  font-style: normal;
+  line-height: 20px;
+  cursor: default;
+`;
+
+export const HeadLine = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 22px;
+`;
+
+export const HeadLineElement = styled.span`
+  color: var(--gray-500, #72787f);
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px;
+  cursor: pointer;
+`;
+
+export const Info = styled.div`
+  display: flex;
+  align-items: center;
+  height: 25px;
+  h2 {
+    font-weight: 500;
+    color: #72787f;
+    font-size: 15px;
+    margin-right: 5px;
+  }
+  span {
+    color: #9ea4aa;
+    font-size: 15px;
+  }
+`;
+
+export const Flex = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  h2 {
+    margin: 0;
+  }
+`;
+
+export const CopyRight = styled.h2`
+  color: var(—gray-500, #72787f);
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px;
+`;
