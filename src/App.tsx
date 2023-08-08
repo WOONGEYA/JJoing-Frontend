0d2881ff2d2ed17e85b6ddb7714bf651a965ed85
lsj0202 <w1004123@gmail.com>
2023-08-08 22:50:10 +0900

    CHORE : delete import react

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from 'pages/LoginPage';
