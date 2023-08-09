578a9adb9533201dfa0af0ae15184895d373073e
sebanimm <gks32114221@gmail.com>
2023-08-09 17:18:34 +0900

    UPDATE : globalstyle

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -10,6 +10,12 @@ input {
 	outline: none;
 }
 
+textarea {
+  font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+  border: none;
+  outline: none;
+}
+
 :root {
   font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
 }
