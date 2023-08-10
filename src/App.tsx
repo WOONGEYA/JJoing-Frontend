97f82e721d627236bdfd5db1935c33de53f2f8c0
HeeSungLee <102288399+lheesung@users.noreply.github.com>
2023-08-10 16:04:45 +0900

    FEAT: 알림 전체/개별 삭제 기능

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import Notifi from 'pages/Notifi';
+import Notify from 'pages/Notify';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const queryClient = new QueryClient();
 
 const App = () => {
-
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-      <Routes>
-        <Route path='/notifi' element={<Notifi />}/>
-      </Routes>
-    </BrowserRouter>
+        <Routes>
+          <Route path='/notify' element={<Notify />} />
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   );
 };
---
+++ src/pages/Notify/index.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import Header from 'components/Header';
+import * as S from './style';
+import * as Flex from 'styles/flex';
+import searchIcon from 'assets/search.svg';
+import trash from 'assets/trash.svg';
+import NotifyBox from 'components/NotifyBox';
+import notifications from 'fixtures/notify.dummy';
+
+interface Notification {
+  user: string;
+  project: string;
+  timestamp: string;
+}
+
+function Notify() {
+  const [notificationData, setNotificationData] = useState<Notification[]>(notifications);
+
+  function handleDeleteAll() {
+    setNotificationData([]);
+  }
+
+  function handleDeleteOne(index: number) {
+    const Notifications = [...notificationData];
+    Notifications.splice(index, 1);
+    setNotificationData(Notifications);
+  }
+
+  return (
+    <>
+      <Header />
+      <S.Container>
+        <S.NotifiHeader>
+          <S.SearchWrapper>
+            <S.Icon src={searchIcon} alt='Search' />
+            <S.Search type='text' placeholder='검색어를 입력해주세요.' />
+          </S.SearchWrapper>
+          <Flex.FlexVertical style={{ gap: '12px' }}>
+            <S.NotifiAmount>알림 {notificationData.length} / 100</S.NotifiAmount>
+            <S.DeleteNotifi onClick={handleDeleteAll}>
+              <S.Icon src={trash} alt='Trash' />
+              모든 알림 삭제하기
+            </S.DeleteNotifi>
+          </Flex.FlexVertical>
+        </S.NotifiHeader>
+        <S.Notifications>
+          {notificationData.map((notification, index) => (
+            <NotifyBox notification={notification} key={index} onDelete={() => handleDeleteOne(index)} />
+          ))}
+        </S.Notifications>
+      </S.Container>
+    </>
+  );
+}
+
+export default Notify;
