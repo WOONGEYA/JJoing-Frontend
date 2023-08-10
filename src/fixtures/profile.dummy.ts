499f5f7aecc0a697858aa870f4a0050691964b2e
sebanimm <gks32114221@gmail.com>
2023-08-10 20:08:26 +0900

    ADD : 더미데이터 추가

---
+++ src/fixtures/profile.dummy.ts
@@ -0,0 +1,11 @@
+const profile_data = {
+  id: 'lheesung',
+  github: 'https://github.com/lheesung',
+  email: 'lheesung@gmail.com',
+  personalLink:
+    'https://lol.ps/summoner/%EC%84%B8%EC%9B%94%EC%9D%80%20%ED%9D%90%EB%A5%BC%20%EB%BF%90?region=kr',
+  statusMessage: '카더가든 사모중 저랑 사귀실분',
+  field: 'Frontend',
+};
+
+export default profile_data;
