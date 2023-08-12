60985d9b3f898b94630131eb1eeb3f4f68f2efe3
sebanimm <gks32114221@gmail.com>
2023-08-12 22:43:09 +0900

    UPDATE : icons

--- src/assets/LinkIcon.tsx
+++ src/assets/LinkIcon.tsx
@@ -11,6 +11,7 @@ const LinkIcon = ({
   width = 24,
   height = 24,
   color = theme.grey[500],
+  ...rest
 }: SVGAttributeProps) => {
   return (
     <svg
@@ -19,6 +20,7 @@ const LinkIcon = ({
       height={height}
       viewBox='0 0 24 24'
       fill='none'
+      {...rest}
     >
       <path
         d='M22 6C22 4.9 21.1 4 20 4H4C2.9 4 2 4.9 2 6V18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V6ZM20 6L12 11L4 6H20ZM20 18H4V8L12 13L20 8V18Z'
