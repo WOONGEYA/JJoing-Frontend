5ae7db191ec0edf1be2c8e584500519571d075f4
sebanimm <gks32114221@gmail.com>
2023-08-12 19:40:29 +0900

    FIX : dropdown

--- src/components/Dropdown/style.ts
+++ src/components/Dropdown/style.ts
@@ -2,19 +2,55 @@ import styled from 'styled-components';
 import theme from 'styles/theme';
 import { font } from 'styles/font';
 
-export const Container = styled.div`
-  box-sizing: border-box;
-  border: 1px solid ${theme.grey[500]};
-  border-radius: 15px;
-  padding: 3px 5px 3px 11px;
+export const DropdownContainer = styled.div`
+  position: relative;
+  background-color: ${theme.white};
+  border-radius: 4px;
+  box-shadow: 0px 1px 4px 0px rgba(0, 0, 0, 0.2);
+  user-select: none;
+  width: 150px;
 `;
 
-export const OptionWrapper = styled.div`
+export const Container = styled.div`
+  box-sizing: border-box;
+  cursor: pointer;
+  padding: 8px 8px 8px 16px;
+  transition: 0.2s ease-in-out;
+  &:hover {
+    background-color: ${theme.grey[200]};
+  }
+`;
+
+export const DropdownWrapper = styled.div`
   display: flex;
   gap: 4px;
   align-items: center;
+  justify-content: space-between;
 `;
 
-export const OptionName = styled.span`
-  ${font.$subhead}
+export const CurrentOption = styled.span`
+  ${font.$callout}
+  ${theme.black}
+`;
+
+export const Options = styled.div`
+  position: absolute;
+  top: 100%;
+  width: 100%;
+  margin-top: 6px;
+  z-index: 2;
+  background-color: ${theme.white};
+  box-shadow: 0px 1px 4px 0px rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+`;
+
+export const Option = styled.div`
+  color: ${theme.black};
+  padding: 8px 16px;
+  cursor: pointer;
+  ${font.$callout};
+  transition: 0.2s ease-in-out;
+  &:hover {
+    background-color: ${theme.grey[200]};
+  }
 `;
