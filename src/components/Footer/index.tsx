d0742ab990e70b0d18cdfc2ef10518e02b9e6553
sebanimm <gks32114221@gmail.com>
2023-08-12 22:20:46 +0900

    UPDATE : Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,8 @@
-import * as S from './style';
 import LogoIcon from 'assets/LogoIcon';
+import * as S from './style';
 
 const Footer = () => {
-  const { service, headline, tel, mail, serviceInfo, locate, copyright } = {
+  const { service, headline, tel, email, founders, location, copyright } = {
     service: '소마고 연합 사이드 프로젝트 모집 플랫폼',
     headline: [
       '이용약관',
@@ -11,44 +11,43 @@ const Footer = () => {
       '문의하기',
       'FAQ | QNA',
     ],
-    tel: '010 - 1234 - 5678',
-    mail: 'jjoing.woong@gmail.com',
-    serviceInfo: '강웅빈 . 고윤영 . 이상진 . 이희성 . 한예준',
-    locate: '부산광역시 강서구 가락대로 1393',
+    tel: 'Tel : 010 - 1234 - 5678',
+    email: 'Email : jjoing.woong@gmail.com',
+    founders: '공동대표 : 강웅빈 . 고윤영 . 이상진 . 이희성 . 한예준',
+    location: '위치 : 부산광역시 강서구 가락대로 1393',
     copyright: 'Copyright Woong ltd. Alright reserved.',
   };
 
   return (
-    <S.Container>
-      <LogoIcon />
-      <S.List>
-        <S.Title>{service}</S.Title>
-        <S.HeadLine>
-          {headline.map((item, index) => (
-            <S.HeadLineElement key={index}>{item}</S.HeadLineElement>
-          ))}
-        </S.HeadLine>
-      </S.List>
-      <S.Info>
-        <h2>Tel:</h2>
-        <span>{tel}</span>
-      </S.Info>
-      <S.Info>
-        <h2>E-mail:</h2>
-        <span>{mail}</span>
-      </S.Info>
-      <S.Info>
-        <h2>공동대표:</h2>
-        <span>{serviceInfo}</span>
-      </S.Info>
-      <S.Flex>
-        <S.Info>
-          <h2>위치:</h2>
-          <span>{locate}</span>
-        </S.Info>
-        <S.CopyRight>{copyright}</S.CopyRight>
-      </S.Flex>
-    </S.Container>
+    <S.FooterContainer>
+      <S.FooterWrapper>
+        <S.InformationContainer>
+          <div>
+            <S.LogoContainer>
+              <LogoIcon width={122} height={58} />
+              <S.Service>{service}</S.Service>
+            </S.LogoContainer>
+            <div>
+              <S.Headlines>
+                {headline.map((data) => (
+                  <S.Headline key={data}>{data}</S.Headline>
+                ))}
+              </S.Headlines>
+              <S.Line />
+            </div>
+          </div>
+          <S.Information>
+            <div>
+              <S.Tel>{tel}</S.Tel>
+              <S.Email>{email}</S.Email>
+              <S.Founders>{founders}</S.Founders>
+              <S.Location>{location}</S.Location>
+            </div>
+            <S.Copyright>{copyright}</S.Copyright>
+          </S.Information>
+        </S.InformationContainer>
+      </S.FooterWrapper>
+    </S.FooterContainer>
   );
 };
 
