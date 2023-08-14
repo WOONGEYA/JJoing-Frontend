d2853562fe126e568341c80776c8560b07c7da5d
sebanimm <gks32114221@gmail.com>
2023-08-14 14:01:54 +0900

    UPDATE : profile dummy data

--- src/fixtures/profile.dummy.ts
+++ src/fixtures/profile.dummy.ts
@@ -7,7 +7,7 @@ const profile_data = {
   email: 'lheesung@gmail.com',
   personalLink:
     'https://lol.ps/summoner/%EC%84%B8%EC%9B%94%EC%9D%80%20%ED%9D%90%EB%A5%BC%20%EB%BF%90?region=kr',
-  statusMessage: '카더가든 사모중 저랑 사귀실분',
+  statusMessage: '카더가든 사모중\n저랑 사귀실분',
   field: 'Frontend',
 };
 
