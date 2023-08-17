fbbf986ca1cf7e4c22176aa573d6d9a13cd7beb3
sebanimm <gks32114221@gmail.com>
2023-08-17 10:13:25 +0900

    UPDATE : 다른사람 마이페이지

--- src/pages/OthersPage/index.tsx
+++ src/pages/OthersPage/index.tsx
@@ -1,90 +1,146 @@
-import Github from 'assets/GithubIcon';
-import dummy from 'fixtures/detail.dummy';
-import Header from 'components/Header';
-import Footer from 'components/Footer';
-import useModal from 'hooks/useModal';
-import ProfileUpdateModal from 'components/ProfileUpdataModal';
+import React from 'react';
+import GithubIcon from 'assets/GithubIcon';
+import LinkIcon from 'assets/LinkIcon';
+import EmailIcon from 'assets/EmailIcon';
+import profile from 'assets/profile.webp';
 import ProjectBox from 'components/ProjectBox';
+import Layout from 'components/Layout';
+import Button from 'components/Button';
+import Tooltip from 'components/Tooltip';
+import Input from 'components/Input';
+import theme from 'styles/theme';
+import { Link } from 'react-router-dom';
+import dummy from 'fixtures/detail.dummy';
+import profile_data from 'fixtures/profile.dummy';
 import * as S from './style';
 
 const MyPage = () => {
-  const { openModal, closeModal } = useModal();
+  const [selected, setSelected] = React.useState(0);
+  const [followed, setFollowed] = React.useState(false);
 
-  const modalOpen = () => {
-    openModal({
-      component: <ProfileUpdateModal closeModal={closeModal} />,
-    });
+  const {
+    id,
+    school,
+    follower,
+    following,
+    github,
+    email,
+    personalLink,
+    statusMessage,
+    field,
+  } = profile_data;
+
+  const copyTooltipText = (text: string) => {
+    navigator.clipboard.writeText(text).then(() => alert('복사 완료'));
+  };
+
+  const handleTabSelect = (e: React.MouseEvent<HTMLDivElement>) => {
+    const id = parseInt(e.currentTarget.id);
+    setSelected(id);
   };
 
   return (
-    <>
-      <Header />
-      <S.Wrapper>
-        <S.ProfileWrapper>
-          <S.ProfileBox>
-            <S.Profile onClick={modalOpen}>
-              <S.Circle />
-            </S.Profile>
-            <S.InformationWrapper>
-              <S.InformationContainer>
-                <S.CharacterName>
-                  뚱이
-                  <Github />
-                </S.CharacterName>
-                <S.SchoolName>부산소프트웨어마이스터고등학교</S.SchoolName>
-                <S.FollowWrapper>
-                  <S.FollowerDiv>팔로워 251</S.FollowerDiv>
-                  <S.FollowDiv>팔로우 317</S.FollowDiv>
-                </S.FollowWrapper>
-                <S.CharacterProfileInformation>
-                  프론트엔드 개발자 지망생, 18살 남자입니다.
-                </S.CharacterProfileInformation>
-              </S.InformationContainer>
-            </S.InformationWrapper>
-          </S.ProfileBox>
-          <S.EditWrapper>
-            <S.EditBox>
-              <S.EditName>팔로우</S.EditName>
-            </S.EditBox>
-          </S.EditWrapper>
-        </S.ProfileWrapper>
-        <S.MyProjectWrapper>
-          <S.ShowMyProject>참여중인 프로젝트</S.ShowMyProject>
-          <S.ProjectNavLine />
-        </S.MyProjectWrapper>
-        <S.ProjectWrapper>
-          <S.ShowProjects>
-            {dummy.map((data) => (
-              <ProjectBox
-                key={data.id}
-                title={data.title}
-                description={data.description}
-                currentPeople={data.currentPeople}
-                requiredPeople={data.requiredPeople}
+    <Layout>
+      <S.Contents>
+        <S.UserContainer>
+          <S.UserInformation>
+            <S.UserWrapper>
+              <S.UserImage>
+                <S.Image
+                  style={{ cursor: 'default' }}
+                  src={profile}
+                  alt='profile'
+                />
+              </S.UserImage>
+              <S.UserData>
+                <div>
+                  <S.UserName>{id}</S.UserName>
+                  <S.UserPosition>
+                    {school} / {field}
+                  </S.UserPosition>
+                </div>
+                <S.Follow>
+                  팔로워 {follower} 팔로잉 {following}
+                </S.Follow>
+                <S.StatusMessage>{statusMessage}</S.StatusMessage>
+              </S.UserData>
+            </S.UserWrapper>
+            <S.ButtonContainer>
+              <Button
+                value={followed ? '언팔로우' : '팔로우'}
+                background={followed ? theme.secondary : theme.primary}
+                onClick={() => setFollowed(!followed)}
               />
+            </S.ButtonContainer>
+          </S.UserInformation>
+          <S.UserLinks>
+            <Link to={github}>
+              <GithubIcon />
+            </Link>
+            <Tooltip
+              value={email}
+              onClick={() => copyTooltipText(email)}
+              style={{ cursor: 'pointer' }}
+            >
+              <Link to={`mailto:${email}`}>
+                <EmailIcon />
+              </Link>
+            </Tooltip>
+            <Link to={personalLink}>
+              <LinkIcon />
+            </Link>
+          </S.UserLinks>
+        </S.UserContainer>
+        <S.TabContainer>
+          <S.TabWrapper>
+            <S.Tab id={0} selected={selected} onClick={handleTabSelect}>
+              참여중인 프로젝트
+            </S.Tab>
+            <S.Tab id={1} selected={selected} onClick={handleTabSelect}>
+              참여했던 프로젝트
+            </S.Tab>
+            <S.Underline selected={selected} />
+          </S.TabWrapper>
+          <S.Search>
+            <Input
+              type='search'
+              width={296}
+              placeholder='검색어를 입력해주세요.'
+            />
+          </S.Search>
+        </S.TabContainer>
+        <S.Projects>
+          {selected === 0 &&
+            (dummy ? (
+              dummy.map((data) => (
+                <ProjectBox
+                  key={data.id}
+                  title={data.title}
+                  description={data.description}
+                  currentPeople={data.currentPeople}
+                  requiredPeople={data.requiredPeople}
+                />
+              ))
+            ) : (
+              <S.NoContents>참여중인 프로젝트가 없습니다.</S.NoContents>
             ))}
-          </S.ShowProjects>
-        </S.ProjectWrapper>
-        <S.MyProjectWrapper>
-          <S.ShowMyProject>참여했던 프로젝트</S.ShowMyProject>
-          <S.ProjectNavLine />
-        </S.MyProjectWrapper>
-        <S.ProjectWrapper>
-          <S.ShowProjects>
-            {dummy.map((data) => (
-              <ProjectBox
-                key={data.id}
-                title={data.title}
-                description={data.description}
-                currentPeople={data.currentPeople}
-                requiredPeople={data.requiredPeople}
-              />
+          {selected === 1 &&
+            (dummy ? (
+              dummy.map((data) => (
+                <ProjectBox
+                  key={data.id}
+                  title={data.title}
+                  description={data.description}
+                  currentPeople={data.currentPeople}
+                  requiredPeople={data.requiredPeople}
+                />
+              ))
+            ) : (
+              <S.NoContents>참여했던 프로젝트가 없습니다.</S.NoContents>
             ))}
-          </S.ShowProjects>
-        </S.ProjectWrapper>
-      </S.Wrapper>
-      <Footer />
-    </>
+        </S.Projects>
+      </S.Contents>
+    </Layout>
   );
 };
 
