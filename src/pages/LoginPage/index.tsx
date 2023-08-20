0fe1cbbe2f3e1f5a475e6ae1b904ec532c1d5afb
sebanimm <gks32114221@gmail.com>
2023-08-20 16:03:19 +0900

    DELETE : instance, URL, import 제거

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,8 +1,6 @@
 import { useLocation, useNavigate } from 'react-router-dom';
-import axios from 'axios';
 import { useEffect } from 'react';
-
-const API_URL = process.env.REACT_APP_API_URL;
+import instance from 'apis/httpClient';
 
 const LoginPage = () => {
   const navigate = useNavigate();
@@ -12,10 +10,6 @@ const LoginPage = () => {
   const encodedCode = searchParams.get('code') ?? '';
 
   const encodedValue = encodeURIComponent(encodedCode);
-
-  const instance = axios.create({
-    baseURL: API_URL,
-  });
 
   const postCode = async (encodedValue: string) => {
     try {
