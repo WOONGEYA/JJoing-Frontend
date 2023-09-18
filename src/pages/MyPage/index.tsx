1f814be4de8982841b6b23c8356c464f127857e4
lsj0202 <w1004123@gmail.com>
2023-09-18 15:42:32 +0900

    FEAT : add myPage 검색기능

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -39,6 +39,7 @@ const MyPage = () => {
   const [userProfile, setUserProfile] = React.useState<UserProfile | null>(
     null,
   );
+  const [userInput, setUserInput] = React.useState<string>('');
 
   const { openModal, closeModal } = useModal();
 
@@ -87,120 +88,86 @@ const MyPage = () => {
       });
   }, []);
 
+  // Modify the project filtering logic to use userInput state
+  const filteredProjects = myProject
+    ? myProject.filter((project) => {
+        const projectName = project.name.toLowerCase();
+        const projectContent = project.content.toLowerCase();
+        const searchQuery = userInput.toLowerCase();
+
+        return (
+          projectName.includes(searchQuery) ||
+          projectContent.includes(searchQuery)
+        );
+      })
+    : [];
+
   return (
     <Layout>
       <S.Contents>
         <S.UserContainer>
-          <S.UserInformation>
-            <S.UserWrapper>
-              <S.UserImage>
-                <S.Image
-                  onClick={modalOpen}
-                  src={userProfile?.imgUrl}
-                  alt='profile'
-                />
-              </S.UserImage>
-              <S.UserData>
-                <div>
-                  <S.UserName>
-                    {userProfile?.name}
-                    {userProfile?.nickName ? (
-                      <S.UserNickName>{userProfile?.nickName}</S.UserNickName>
-                    ) : (
-                      <S.UserNickName>(닉네임을 추가해주세요)</S.UserNickName>
-                    )}
-                  </S.UserName>
-                  <S.UserPosition>
-                    {userProfile?.school} /{' '}
-                    {userProfile?.major
-                      ? userProfile?.major
-                      : '(분야를 추가해주세요)'}
-                  </S.UserPosition>
-                </div>
-                <S.StatusMessage>
-                  {userProfile?.statusMessage
-                    ? userProfile?.statusMessage
-                    : '(상태 메시지를 추가해주세요)'}
-                </S.StatusMessage>
-              </S.UserData>
-            </S.UserWrapper>
-            <S.ButtonContainer>
-              <Button value='프로필 편집하기' onClick={modalOpen} />
-            </S.ButtonContainer>
-          </S.UserInformation>
-          <S.UserLinks>
-            <Link to={String(userProfile?.githubUrl)}>
-              <GithubIcon />
-            </Link>
-            <Tooltip
-              value={String(userProfile?.email)}
-              onClick={() => copyTooltipText(String(userProfile?.email))}
-              style={{ cursor: 'pointer' }}
-            >
-              <Link to={`mailto:${String(userProfile?.email)}`}>
-                <EmailIcon />
-              </Link>
-            </Tooltip>
-          </S.UserLinks>
+          {/* ... */}
+          <S.TabContainer>
+            <S.TabWrapper>
+              <S.Tab id={0} selected={selected} onClick={handleTabSelect}>
+                참여중인 프로젝트
+              </S.Tab>
+              <S.Tab id={1} selected={selected} onClick={handleTabSelect}>
+                참여했던 프로젝트
+              </S.Tab>
+              <S.Underline selected={selected} />
+            </S.TabWrapper>
+            <S.Search>
+              <Input
+                type='search'
+                width={296}
+                placeholder='검색어를 입력해주세요.'
+                value={userInput} // Pass user input value
+                onChange={(e) => setUserInput(e.target.value)} // Update user input state
+              />
+            </S.Search>
+          </S.TabContainer>
+          <S.Projects>
+            {selected === 0 && filteredProjects.length > 0 ? (
+              filteredProjects
+                .slice()
+                .sort((a, b) => b.id - a.id)
+                .map((data) => (
+                  <ProjectBox
+                    id={data.id}
+                    key={data.id}
+                    name={data.name}
+                    content={data.content}
+                    currentPeople={data.currentPeople}
+                    requiredPeople={data.requiredPeople}
+                    imgUrl={data.imgUrl}
+                    viewCount={data.viewCount}
+                  />
+                ))
+            ) : selected === 0 ? (
+              <S.NoContents>참여중인 프로젝트가 없습니다.</S.NoContents>
+            ) : null}
+            {selected === 1 && filteredProjects.length > 0 ? (
+              filteredProjects
+                .slice()
+                .sort((a, b) => b.id - a.id)
+                .map((data) => (
+                  <ProjectBox
+                    id={data.id}
+                    key={data.id}
+                    name={data.name}
+                    content={data.content}
+                    currentPeople={data.currentPeople}
+                    requiredPeople={data.requiredPeople}
+                    imgUrl={data.imgUrl}
+                    viewCount={data.viewCount}
+                  />
+                ))
+            ) : selected === 1 ? (
+              <S.NoContents>참여했던 프로젝트가 없습니다.</S.NoContents>
+            ) : null}
+          </S.Projects>
         </S.UserContainer>
-        <S.TabContainer>
-          <S.TabWrapper>
-            <S.Tab id={0} selected={selected} onClick={handleTabSelect}>
-              참여중인 프로젝트
-            </S.Tab>
-            <S.Tab id={1} selected={selected} onClick={handleTabSelect}>
-              참여했던 프로젝트
-            </S.Tab>
-            <S.Underline selected={selected} />
-          </S.TabWrapper>
-          <S.Search>
-            <Input
-              type='search'
-              width={296}
-              placeholder='검색어를 입력해주세요.'
-            />
-          </S.Search>
-        </S.TabContainer>
-        <S.Projects>
-          {selected === 0 && myProject && myProject.length > 0 ? (
-            myProject
-              .slice() // 원본 배열을 변경하지 않기 위해 복사본을 만듭니다.
-              .sort((a, b) => b.id - a.id) // id를 역순으로 정렬합니다.
-              .map((data) => (
-                <ProjectBox
-                  id={data.id}
-                  key={data.id}
-                  name={data.name}
-                  content={data.content}
-                  currentPeople={data.currentPeople}
-                  requiredPeople={data.requiredPeople}
-                  imgUrl={data.imgUrl}
-                  viewCount={data.viewCount}
-                />
-              ))
-          ) : selected === 0 ? (
-            <S.NoContents>참여중인 프로젝트가 없습니다.</S.NoContents>
-          ) : null}
-          {selected === 1 && myProject && myProject.length > 0 ? (
-            myProject
-              .slice() // 원본 배열을 변경하지 않기 위해 복사본을 만듭니다.
-              .sort((a, b) => b.id - a.id) // id를 역순으로 정렬합니다.
-              .map((data) => (
-                <ProjectBox
-                  id={data.id}
-                  key={data.id}
-                  name={data.name}
-                  content={data.content}
-                  currentPeople={data.currentPeople}
-                  requiredPeople={data.requiredPeople}
-                  imgUrl={data.imgUrl}
-                  viewCount={data.viewCount}
-                />
-              ))
-          ) : selected === 1 ? (
-            <S.NoContents>참여했던 프로젝트가 없습니다.</S.NoContents>
-          ) : null}
-        </S.Projects>
       </S.Contents>
     </Layout>
   );
