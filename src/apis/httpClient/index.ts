cbf0ccaf7f640eb4c48de46f97aaea0e7b1685d7
sebanimm <gks32114221@gmail.com>
2023-09-20 19:07:32 +0900

    ADD : 액세스토큰 재발급 로직 추가

--- src/apis/httpClient/index.ts
+++ src/apis/httpClient/index.ts
@@ -29,6 +29,36 @@ instance.interceptors.response.use(
   },
 
   (error) => {
+    const { status } = error.response;
+
+    const getUpdatedAccessToken = async () => {
+      const refreshToken = localStorage.getItem('refreshToken');
+      const { data } = await axios.put(`${API_URL}/login`, null, {
+        headers: {
+          'Refresh-Token': refreshToken,
+        },
+      });
+
+      return data;
+    };
+
+    const setUpdatedAccessToken = (accessToken: string) => {
+      localStorage.setItem('accessToken', accessToken);
+    };
+
+    const handleAccessTokenRequest = async () => {
+      try {
+        const { accessToken } = await getUpdatedAccessToken();
+        setUpdatedAccessToken(accessToken);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (status === 403) {
+      handleAccessTokenRequest();
+    }
+
     return error;
   },
 );
