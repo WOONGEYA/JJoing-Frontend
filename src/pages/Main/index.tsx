9d430187a679879cb53b655fa323a9b197f17ced
lsj0202 <w1004123@gmail.com>
2023-10-05 04:14:59 +0900

    FIX : 불필요한 페이지 삭제

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -68,27 +68,6 @@ const Main = () => {
             boxShadow: 'none',
           }}
         >
-          <S.Title>쪼잉은 이런게 좋아요</S.Title>
-          <S.Content>
-            <S.Photo></S.Photo>
-            <S.TextArea>
-              <S.SubTitle style={{ fontWeight: '500' }}>
-                안녕하세요안녕하세요
-              </S.SubTitle>
-              <S.Title2 style={{ fontWeight: '300' }}>
-                안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요
-                안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요
-              </S.Title2>
-            </S.TextArea>
-          </S.Content>
-        </S.MainBox>
-        <S.MainBox
-          style={{
-            padding: '220px 204px',
-            boxSizing: 'border-box',
-            boxShadow: 'none',
-          }}
-        >
           <S.Title>어떻게 진행되나요?</S.Title>
           <S.Title2 style={{ fontWeight: '200' }}>
             혼자라서 미뤄왔던 프로젝트를 쪼잉으로 도전해보세요 ! 자신의 분야를
