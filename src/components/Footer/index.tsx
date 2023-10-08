7f06f51d4bbffa920f214be5a88d5349fb7b39c7
sebanimm <gks32114221@gmail.com>
2023-10-09 00:15:18 +0900

    UPDATE : 푸터 수정

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,51 +1,28 @@
 import LogoIcon from 'assets/LogoIcon';
+import GithubIcon from 'assets/GithubIcon';
 import * as S from './style';
 
-const Footer = () => {
-  const { service, headline, tel, email, founders, location, copyright } = {
-    service: '소마고 연합 사이드 프로젝트 모집 플랫폼',
-    headline: [
-      '이용약관',
-      '개인정보처리방침',
-      '공지사항',
-      '문의하기',
-      'FAQ | QNA',
-    ],
-    tel: 'Tel : 010 - 1234 - 5678',
-    email: 'Email : jjoing.woong@gmail.com',
-    founders: '공동대표 : 강웅빈 . 고윤영 . 이상진 . 이희성 . 한예준',
-    location: '위치 : 부산광역시 강서구 가락대로 1393',
-    copyright: 'Copyright Woong ltd. Alright reserved.',
-  };
+const WOONGEYA_GITHUB = 'https://github.com/WOONGEYA';
 
+const Footer = () => {
   return (
     <S.FooterContainer>
       <S.FooterWrapper>
-        <S.InformationContainer>
-          <div>
+        <S.Content>
+          <S.Top>
             <S.LogoContainer>
-              <LogoIcon width={122} height={58} />
-              <S.Service>{service}</S.Service>
+              <LogoIcon width={92} height={43} />
+              <S.Service>사이드 프로젝트 모집 플랫폼</S.Service>
             </S.LogoContainer>
-            <div>
-              <S.Headlines>
-                {headline.map((data) => (
-                  <S.Headline key={data}>{data}</S.Headline>
-                ))}
-              </S.Headlines>
-              <S.Line />
-            </div>
-          </div>
-          <S.Information>
-            <div>
-              <S.Tel>{tel}</S.Tel>
-              <S.Email>{email}</S.Email>
-              <S.Founders>{founders}</S.Founders>
-              <S.Location>{location}</S.Location>
-            </div>
-            <S.Copyright>{copyright}</S.Copyright>
-          </S.Information>
-        </S.InformationContainer>
+            <a href={WOONGEYA_GITHUB} target='_blank' rel='noreferrer'>
+              <GithubIcon cursor='pointer' width={64} height={64} />
+            </a>
+          </S.Top>
+          <S.Bottom>
+            <S.Team>팀 웅이야 | 부산소프트웨어마이스터고등학교</S.Team>
+            <S.Copyright>Copyright Woong ltd. All rights reserved.</S.Copyright>
+          </S.Bottom>
+        </S.Content>
       </S.FooterWrapper>
     </S.FooterContainer>
   );
