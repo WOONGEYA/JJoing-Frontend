23ea30560e2174943a1e699b1d696f7217b598d5
sebanimm <gks32114221@gmail.com>
2023-10-15 18:45:45 +0900

    FIX : 이상한 폰트 수정

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -13,9 +13,8 @@ const fontGenerator = (
 export const font = {
   $titleLarge: fontGenerator(700, 32, 150, 0),
   $title01: fontGenerator(700, 26, 150, 0),
-  $title02: fontGenerator(700, 20, 150, 0),
-  $title03: fontGenerator(600, 18, 150, 0),
-  $title04: fontGenerator(500, 13, 150, 0),
+  $title02: fontGenerator(700, 22, 150, 0),
+  $title03: fontGenerator(600, 20, 150, 0),
   $body: fontGenerator(500, 16, 150, 0),
   $headline: fontGenerator(500, 15, 150, 0),
   $callout: fontGenerator(400, 14, 150, 0),
