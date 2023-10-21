d09fe9b1977d0d1a916b55c4b3bc51988ce5602f
sebanimm <gks32114221@gmail.com>
2023-10-21 19:40:16 +0900

    FIX

--- src/assets/png/index.ts
+++ src/assets/png/index.ts
@@ -4,4 +4,4 @@ export { default as tab2 } from './tab02.png';
 export { default as tab3 } from './tab03.png';
 export { default as card1 } from './card01.png';
 export { default as card2 } from './card02.png';
-export { default as card3 } from './card.png';
+export { default as card3 } from './card03.png';
